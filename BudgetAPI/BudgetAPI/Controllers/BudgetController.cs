e16557de98e72aee7b3acbfa71104f962afc881b
Jonathan De La Cruz <jdelacruznode@gmail.com>
2022-08-28 19:40:22 -0400

    adding scaffolding

---
+++ BudgetAPI/BudgetAPI/Controllers/BudgetController.cs
@@ -0,0 +1,36 @@
+﻿using BudgetAPI.Data;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BudgetAPI.Controllers
+{
+    [EnableCors("_myAllowSpecificOrigins")]
+    [Route("api/budget")]
+    [ApiController]
+    public class BudgetController : ControllerBase
+    {
+        private readonly IBudgetRepo _repo;
+        private readonly ILogger<BudgetController> _logger;
+
+        public BudgetController(IBudgetRepo repo, ILogger<BudgetController> logger)
+        {
+            _repo = repo;
+            _logger = logger;
+        }
+
+        //CRUD METHODS
+        //GET BUDGET
+
+
+        //GET BUDGETS FOR ALL CUSTOMER ACCOUNT
+
+        //POST BUDGET 
+
+        //PUT BUDGET
+
+        //DELETE BUDGET
+
+    }
+}
--- BudgetAPI/BudgetAPI/Data/BudgetSQLRepo.cs
+++ BudgetAPI/BudgetAPI/Data/BudgetSQLRepo.cs
@@ -1,7 +1,12 @@
-﻿namespace BudgetAPI.Data
+﻿using System.Data.SqlClient;
+using Microsoft.Extensions.Logging;
+using BudgetAPI.Models;
+
+namespace BudgetAPI.Data
 {
     public class BudgetSQLRepo : IBudgetRepo
     {
+        //Fields
         public readonly string _connectionString;
 
         private readonly ILogger<BudgetSQLRepo> _logger;
@@ -11,6 +16,18 @@
             _connectionString = connectionString;
             _logger = logger;
         }
+
+        //Methods
+
+        //Get one budget by acount_id
+
+        //Get all budgets for a customer
+
+        //Post new budget
+
+        //Put budget to update a budget
+
+        //Delete a budget, by acountId
 
     }
 }
